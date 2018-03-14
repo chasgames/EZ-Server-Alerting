e9285223ee6e032bba2f308014df6f97734231c3
dezmk <test@de.z.mk>
2018-03-14 16:19:34 +0100

    changed hash to diff

--- mon_passwd.sh
+++ mon_passwd.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
 source settings.conf
 
-CURRENTHASH=$(sha256sum /etc/passwd)
-OLDHASH=$(cat /var/tmp/passwdcheck)
+#CURRENTHASH=$(sha256sum /etc/passwd)
+#OLDHASH=$(cat /var/tmp/passwdcheck)
 
 if [ "$FIRSTRUN" -eq 1 ]; then
-    echo "$CURRENTHASH" > /var/tmp/passwdcheck
+#    echo "$CURRENTHASH" > /var/tmp/passwdcheck
+cp /etc/passwd /var/tmp/alerting-comp-pw
     sed -i 's/FIRSTRUN=1/FIRSTRUN=0/g' settings.conf
 fi
 
-     
+diff /etc/passwd /var/tmp/alerting-comp-pw
+if [ $? -ne 0 ]; then
+ntfy send "$(
+echo ":key: /etc/passwd has changed, New user has been added or something has changed about a user"
+echo -e "The differences:\n"
+grep -Fxvf /etc/passwd /var/tmp/alerting-comp-pw
+)"
+cp /etc/passwd /var/tmp/alerting-comp-pw
 
-
-if [ "$CURRENTHASH" ==  "$OLDHASH" ]; then
-    echo good
-else
-    echo bad
 fi
+
 
 
 
